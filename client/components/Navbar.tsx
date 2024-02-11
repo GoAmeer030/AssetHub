6887f87618e4c7d595c50ec00ae085d95cc19726
HLrahul <rahuls10org@gmail.com>
2024-02-11 14:45:22 +0530

    Refactor Navbar component

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -4,21 +4,15 @@ import Image from "next/image";
 import { ThemeMenuButton } from "./ThemeMenuButton";
 
 export default function Navbar() {
-    return (
-        <nav className="fixed top-0 mt-5 left-1/2 transform -translate-x-1/2 w-11/12 flex justify-between items-center bg-card py-2 px-6 rounded-lg shadow border border-primary">
-            <div className="flex items-center">
-                <Image
-                    src="/NotesIcon.svg"
-                    alt="Search"
-                    width={20}
-                    height={20}
-                    className="mr-2"
-                />
-                <p className="text-2xl font-bold">Noter</p>
-            </div>
-            <div>
-                <ThemeMenuButton />
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="fixed top-0 w-full py-7 px-10 flex justify-between">
+      <div className="flex">
+        <p className="text-2xl font-bold">Noter</p>
+      </div>
+
+      <div>
+        <ThemeMenuButton />
+      </div>
+    </nav>
+  );
 }
